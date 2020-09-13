5b64175dea112abdda9bead134e6313733841008
Ahmad Ch <ahmad8t8@gmail.com>
2020-09-13 06:04:51 +0500

    Updates

---
+++ precache-manifest.7453e2c3e018a286b3db71761d29c981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a93a75be3823cf7275ba27ef0028aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43d2e1c7b51b24334f7a",
+    "url": "/monsters-rolodex/static/css/main.0c144896.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d2e1c7b51b24334f7a",
+    "url": "/monsters-rolodex/static/js/main.4de162ed.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
